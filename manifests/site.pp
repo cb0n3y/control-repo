4e18f2c7f7b19b9a71c21dcfe08aed83fa93f5d0
cb0n3y <tom4mob@gmail.com>
2021-08-05 09:59:01 +0200

    deleted node default from site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,8 +25,8 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+#node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-}
+#}
